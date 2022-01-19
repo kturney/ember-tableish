d31a7b1a55a31fe150513b9e278ef28ddd9ed74b
Chris Miller <chris.miller@toyotaconnected.com>
2022-01-19 13:45:57 -0600

    move away from elementId

--- addon/components/ember-tableish.js
+++ addon/components/ember-tableish.js
@@ -1,7 +1,16 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { guidFor } from '@ember/object/internals';
 
 export default class EmberTableish extends Component {
+  @tracked tableId;
+
   get columnGap() {
     return this.args.columnGap || null;
+  }
+
+  constructor() {
+    super(...arguments);
+    this.tableId = guidFor(this);
   }
 }
