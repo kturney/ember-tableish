f497babce86668405873954bf5f6f50ec10b3873
Chris Miller <chris.miller@toyotaconnected.com>
2022-01-19 09:56:42 -0600

    v3.13.1...v3.28.5

--- README.md
+++ README.md
@@ -19,9 +19,9 @@ Being grid based allows more flexibility, e.g. the ability to have rows be links
 
 ## Compatibility
 
-* Ember.js v3.4 or above
-* Ember CLI v2.13 or above
-* Node.js v8 or above
+* Ember.js v3.24 or above
+* Ember CLI v3.24 or above
+* Node.js v12 or above
 
 ## Installation
 
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,8 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'dummy/app';
+import config from 'dummy/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 import { Promise as EmberPromise } from 'rsvp';
 
@@ -9,5 +11,7 @@ if (typeof Promise === 'undefined') {
 }
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
